b2a8f22b431793acc274c07b79a6b8074d7cf359
barro97 <rosenthb@post.bgu.ac.il>
2024-09-23 15:40:34 +0300

    frontend for sifn created frontend for signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,66 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import { useState } from "react";
 function App() {
+  return <SignupPage />;
+}
+
+function SignupPage() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    instrument: "",
+  });
+
+  // A function that handles field change:
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSubmit(e) {
+    e.preventdefault();
+    console.log(formData);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>hi</div>
+    <div className="signup-container">
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <h2>Sign Up</h2>
+        <Field
+          type="text"
+          purpose="username"
+          value={formData.username}
+          handleChange={handleChange}
+        />
+        <Field
+          type="password"
+          purpose="password"
+          value={formData.password}
+          handleChange={handleChange}
+        />
+        <Field
+          type="text"
+          purpose="instrument"
+          value={formData.instrument}
+          handleChange={handleChange}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
     </div>
   );
 }
 
+function Field({ type, purpose, value, handleChange }) {
+  return (
+    <>
+      <label htmlFor={purpose}>{purpose}:</label>
+      <input
+        type={type}
+        id={purpose}
+        name={purpose}
+        value={value}
+        onChange={handleChange}
+        required
+      />
+    </>
+  );
+}
 export default App;
