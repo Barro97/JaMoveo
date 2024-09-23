b1b64a5eba52e55c9b63319648c59ef188f56f3d
barro97 <rosenthb@post.bgu.ac.il>
2024-09-23 15:04:32 +0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+/**
+ mongodb+srv://rosenthb:<db_password>@cluster0.j8ajp.mongodb.net/jaMoveo?retryWrites=true&w=majority&appName=Cluster0
+ */
+
+const url =
+  "mongodb+srv://rosenthb:bO5JHYTfa5hFSN3o@cluster0.j8ajp.mongodb.net/jaMoveo?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(url);
+let db;
+// Function to connect to the MongoDB database
+async function connectToDb() {
+  try {
+    await client.connect();
+    db = client.db("jaMoveo"); // Using your actual DB name "Code-togetherDB"
+    console.log("Connected to Jamoveo!");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+  }
+}
+
+connectToDb(); // Call this function to connect to the database
+// Middleware for preventing the browser's default behavior
+app.use(cors());
+
+//Starting the server:
+const PORT = process.env.PORT || 5000; //listening to either a global variable of a port or a default local host
+httpServer.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+});
