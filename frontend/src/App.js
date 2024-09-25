f84c84682601537ea37781355970c946b79b7bf6
barro97 <rosenthb@post.bgu.ac.il>
2024-09-25 11:03:32 +0300

    extracted main to separate file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,8 @@ import {
 } from "react-router-dom";
 import { io } from "socket.io-client";
 import AuthPage from "./components/AuthPage";
-import Search from "./components/Search";
 import LivePage from "./components/LivePage";
+import Main from "./components/Main";
 
 const server = "http://localhost:5000";
 const socket = io("http://localhost:5000");
@@ -27,6 +27,7 @@ function App() {
   });
   const [song, setSong] = useState("");
   const [isSong, setIsSong] = useState(false);
+
   function handleSongSelect(selectedSong) {
     setSong(selectedSong);
     setIsSong(true);
@@ -58,6 +59,7 @@ function App() {
               onSongSelect={handleSongSelect}
               song={song}
               isSong={isSong}
+              socket={socket}
             />
           }
         />
@@ -75,77 +77,6 @@ function App() {
         />
       </Routes>
     </Router>
-  );
-}
-
-function Main({ user, isAdmin, onLogin, onSongSelect, song, isSong }) {
-  const navigate = useNavigate(); // Used to navigate to the main page after logging in
-
-  useEffect(() => {
-    if (song && isSong) socket.emit("song-selected", song);
-  }, [song]);
-
-  useEffect(() => {
-    socket.on("change-page", (selectedSong) => {
-      if (!song) {
-        onSongSelect(selectedSong);
-      }
-      navigate("/song");
-    });
-  });
-  useEffect(() => {
-    socket.on("currentUser", (loggedUser) => {
-      console.log("Received user", loggedUser);
-      // An empty object means this user just logged in
-      if (Object.keys(user).length === 0) {
-        onLogin(loggedUser, loggedUser.type === "admin");
-      }
-    });
-    const handleBeforeUnload = () => {
-      sessionStorage.setItem("user", JSON.stringify(user));
-      sessionStorage.setItem("isAdmin", JSON.stringify(isAdmin));
-    };
-
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
-    return () => {
-      // Clean up the event listener
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, [user, isAdmin, onLogin]);
-  return (
-    <>
-      <Header user={user} isAdmin={isAdmin} />
-      {isAdmin ? (
-        <Search onSongSelect={onSongSelect} />
-      ) : (
-        <h2>Waiting for next song...</h2>
-      )}
-    </>
-  );
-}
-
-function Header({ user, isAdmin }) {
-  const navigate = useNavigate();
-
-  function disconnect() {
-    socket.emit("leaveRoom", user);
-    sessionStorage.removeItem("user", JSON.stringify(user));
-    sessionStorage.removeItem("isAdmin", JSON.stringify(isAdmin));
-    navigate("/");
-  }
-  return (
-    <header>
-      <div className="header-content">
-        <h1>
-          Welcome, {user.username}{" "}
-          <span className="role-text">{isAdmin ? "admin" : "player"}</span>
-        </h1>
-        <button className="disconnect-button" onClick={disconnect}>
-          Disconnect
-        </button>
-      </div>
-    </header>
   );
 }
 
---
+++ frontend/src/components/Main.js
@@ -0,0 +1,76 @@
+import { useEffect } from "react";
+import Search from "./Search";
+import { useNavigate } from "react-router-dom";
+
+function Main({ user, isAdmin, onLogin, onSongSelect, song, isSong, socket }) {
+  const navigate = useNavigate(); // Used to navigate to the main page after logging in
+
+  useEffect(() => {
+    if (song && isSong) socket.emit("song-selected", song);
+  }, [song, isSong, socket]);
+
+  useEffect(() => {
+    socket.on("change-page", (selectedSong) => {
+      if (!song) {
+        onSongSelect(selectedSong);
+      }
+      navigate("/song");
+    });
+  });
+
+  useEffect(() => {
+    socket.on("currentUser", (loggedUser) => {
+      console.log("Received user", loggedUser);
+      // An empty object means this user just logged in
+      if (Object.keys(user).length === 0) {
+        onLogin(loggedUser, loggedUser.type === "admin");
+      }
+    });
+    const handleBeforeUnload = () => {
+      sessionStorage.setItem("user", JSON.stringify(user));
+      sessionStorage.setItem("isAdmin", JSON.stringify(isAdmin));
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      // Clean up the event listener
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [user, isAdmin, onLogin, socket]);
+  return (
+    <>
+      <Header user={user} isAdmin={isAdmin} socket={socket} />
+      {isAdmin ? (
+        <Search onSongSelect={onSongSelect} />
+      ) : (
+        <h2>Waiting for next song...</h2>
+      )}
+    </>
+  );
+}
+
+function Header({ user, isAdmin, socket }) {
+  const navigate = useNavigate();
+
+  function disconnect() {
+    socket.emit("leaveRoom", user);
+    sessionStorage.removeItem("user", JSON.stringify(user));
+    sessionStorage.removeItem("isAdmin", JSON.stringify(isAdmin));
+    navigate("/");
+  }
+  return (
+    <header>
+      <div className="header-content">
+        <h1>
+          Welcome, {user.username}{" "}
+          <span className="role-text">{isAdmin ? "admin" : "player"}</span>
+        </h1>
+        <button className="disconnect-button" onClick={disconnect}>
+          Disconnect
+        </button>
+      </div>
+    </header>
+  );
+}
+export default Main;
